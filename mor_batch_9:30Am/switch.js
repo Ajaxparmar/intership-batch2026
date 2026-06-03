68f333ca9503f65c35d69b0bd3db1fc364166212
Ajaxparmar <ajaxparmar@gmail.com>
2026-06-03 16:09:25 +0530

    first commit

---
+++ mor_batch_9:30Am/switch.js
@@ -0,0 +1,32 @@
+var no = 2;
+switch(no)
+    {
+        case 1:
+            console.log("one");
+            break;
+        case 2:
+            console.log("two");
+            break;
+        case 3:
+            console.log("three");
+            break;
+        default:
+            console.log("invalid");
+    }
+
+    if(no == 1)
+    {
+        console.log("this is form if one");
+    }
+    else if(no == 2)
+    {
+        console.log("this is form if two");
+    }
+    else if(no == 3)
+    {
+        console.log("this is form if three");
+    }
+    else
+    {
+        console.log("this is form if invalid");
+    }
