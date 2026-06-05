c798446a385a834d8b912ebb3b0723cda2f267fd
unknown <parvgarg1609@gmail.com>
2026-06-05 11:48:47 +0530

    vanshika/string.js

--- vanshika/string.js
+++ vanshika/string.js
@@ -1,3 +1,56 @@
-let text = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-let length = text.length;
-console.log(length);
+// let text1 = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+// let length = text1.length;
+// console.log(length);
+
+// let text2 = "HELLO WORLD";
+// console.log(text2.charCodeAt(0));
+
+// let text1 = "Hello";
+// let text2 = "World";
+// let text3 = text1.concat("      ", text2);
+// console.log(text3);
+
+
+// let name = "vanshika, muskan";
+// let part = name.slice(7, 13);
+// console.log(part);
+
+
+// let name1 = "vanshika,muskan,anchal";
+// let part2 = name1.slice(-7);
+// // let part1 = name1.slice(-13, -7);
+// console.log(part1);
+// console.log(part2);
+
+// let text1 = "      Hello    World!                              ";
+// let text2 = text1.trim();
+// console.log(text2);
+
+// let text = "Please visit Microsoft!";
+// let newText = text.replace("Microsoft", "W3Schools");
+// console.log(newText);
+
+// let firstName = "John";
+// let lastName = "Doe";
+
+// let text = `Welcome ${firstName}, ${lastName}!`;
+// console.log(text);
+
+// let header = "Template Strings";
+// let tags = ["template strings", "javascript", "es6"];
+
+// let html = `<h2>${header}</h2><ul>`;
+
+// for (const x of tags) {
+//   html += `<li>${x}</li>`;
+// }
+
+// html += `</ul>`;
+//  console.log(html);
+ 
+
+
+ let text = "Hello";
+const myArr = text.split("");
+ console.log(myArr);
+ 
