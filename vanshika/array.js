e1201b6c388051f9b9014bc2e4266cc62732ca6e
unknown <parvgarg1609@gmail.com>
2026-06-09 14:52:20 +0530

    vanshika/array.js

---
+++ vanshika/array.js
@@ -0,0 +1,75 @@
+// const colour=["red","yellow"];
+// console.log(colour);
+
+
+// const colour=["red","yellow"];
+// console.log(colour.length);
+
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+
+// fruits.length = 2;  //   all element print give position 
+//  fruit = fruits.at(2);// element give give position
+// console.log(fruit);
+
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// let fruit = fruits[2];// same for at 
+
+// console.log(fruit);
+
+// console.log(fruits);
+// let myList = fruits.toString();
+// console.log(myList);
+// // 
+
+// const cars = [];
+// cars[0]= "Saab";
+// cars[1]= "Volvo";
+// cars[2]= "BMW";
+// console.log(cars);
+// car = cars.join("*")
+// console.log(car);
+
+const marks = [78,90,45,34,79];
+// console.log(marks);
+// console.log(marks[2]);
+// marks[1] =60;
+// console.log(marks);
+// console.log(marks.length);
+// console.log(marks.sort());
+
+
+
+
+
+// colour = myArray("Banana", "Orange", "Apple", "Mango");
+// console.log(colour);
+
+ 
+
+
+
+// const colour=["red","yellow","black","golden"];
+// let position = colour.indexOf("black") + 1;
+// console.log(position);
+// console.log(colour.sort());
+// console.log(colour.reverse());
+
+
+
+
+// const colour1=["red","yellow","black","golden","yellow","white"];
+// let position1 = colour1.lastIndexOf("yellow");// shows last index for element
+// console.log(colour1.includes("red"));
+
+// console.log(position1);
+
+
+const fruits3 = ["Banana", "Orange", "Apple", "Mango"];
+console.log(fruits3.sort());
+ console.log(fruits3.reverse());
+
+
+
+const months = ["Jan", "Feb", "Mar", "Apr"];
+const sorted = months.toSorted();
+console.log(sorted);
